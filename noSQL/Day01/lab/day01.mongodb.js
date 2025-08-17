2f729496d2234be26d326ebf2883ca1848adc00e
Hussein Mohamed <hussein7mohamed8@gmail.com>
2025-08-17 13:50:16 +0300

    Create day01.mongodb.js

---
+++ noSQL/Day01/lab/day01.mongodb.js
@@ -0,0 +1,12 @@
+/*
+    Hussein Mohamed
+    Lab 01 - noSQL
+    file structure:
+    command
+    // result
+*/
+
+use('test')
+// mongodb.confirmRunAll
+db.mycol.insertOne({ name: "Hussein", age: 25 })
+db.mycol.find()
